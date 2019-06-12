b04eb75aa8f8b8c4fae02321b68d70b4f6f62cb5
dmueller1607 <dmueller1607@gmail.com>
2019-06-12 14:57:38 +0200

    SQL command for stemming results table creation

---
+++ scripts/db_creation/mysql_script.sql
@@ -0,0 +1,19 @@
+DROP TABLE email_list;
+DROP TABLE email_stemming;
+
+CREATE TABLE email_list (
+    ID int PRIMARY KEY AUTO_INCREMENT,
+    email_path varchar(200) NOT NULL,
+    email_from varchar(100) NOT NULL,
+    email_to varchar(100) NOT NULL,
+    email_subject varchar(200) NOT NULL,
+    email_body varchar(500) NOT NULL,
+    CONSTRAINT c_unique_entry UNIQUE (email_path, email_from, email_to, email_subject, email_body)
+);
+
+CREATE TABLE email_stemming (
+    ID int PRIMARY KEY AUTO_INCREMENT,
+    emailID int NOT NULL,
+    stemming_word varchar(100) NOT NULL,
+    FOREIGN KEY (emailID) REFERENCES email_list(ID)
+);
